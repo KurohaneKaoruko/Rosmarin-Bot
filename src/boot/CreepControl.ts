570e42b601ed7c88129055c66a7329c55cb9c2a1
KurohaneKaoruko <kurohanekaoruko@163.com>
2026-01-15 22:30:35 +0800

    fix: 上一次提交没删完相关代码

--- src/boot/CreepControl.ts
+++ src/boot/CreepControl.ts
@@ -10,16 +10,4 @@ export const creepControl = function (creep: Creep) {
 
     // Creep工作
     creep.exec();
-
-    creep.randomSing();
-
-    const sayText = creep.memory.sayText;
-    if (!sayText) return;
-    if (typeof sayText === 'string') {
-        creep.say(sayText, true);
-    } else if (Array.isArray(sayText)) {
-        const text = creep.memory.sayText.shift();
-        if(text) creep.say(text, true);
-        else creep.memory.sayText = null;
-    } else creep.memory.sayText = null;
 }
