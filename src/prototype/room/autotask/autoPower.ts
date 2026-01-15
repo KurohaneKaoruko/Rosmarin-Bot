490746e41c8dff7d5230e56bd3a3aaf0d7cb49a4
KurohaneKaoruko <kurohanekaoruko@163.com>
2026-01-16 00:21:13 +0800

    refactor(autoPower): 修改自动烧power的判定条件，默认情况下按照默认参数进行烧power

--- src/prototype/room/autotask/autoPower.ts
+++ src/prototype/room/autotask/autoPower.ts
@@ -1,27 +1,30 @@
+import { log } from "@/utils";
+
 export default class AutoPower extends Room {
     autoPower() {
         if (Game.time % 50) return;
 
         const BotMem =  Memory['AutoData']['AutoPowerData'][this.name];
         if (!BotMem) return;
-        if (!BotMem['energy'] || !BotMem['power']) return;
+        
+        const energy = BotMem['energy'] ?? 100e3;
+        const power = BotMem['power'] ?? 10e3;
+        if (energy == 0 && power == 0) return;
 
         const BotMemStruct =  Memory['StructControlData'][this.name];
-
         if (BotMemStruct['powerSpawn'] &&
-            (this[RESOURCE_ENERGY] < BotMem['energy'] ||
-            this[RESOURCE_POWER] <= 0)
+            (this[RESOURCE_ENERGY] < energy || this[RESOURCE_POWER] == 0)
         ) {
             BotMemStruct['powerSpawn'] = false;
-            global.log(`[自动PowerSpawn] ${this.name}资源低于阈值, 已关闭PowerSpawn`);
+            log('AutoPower', `${this.name}资源低于阈值, 已关闭PowerSpawn`);
         }
 
         if (!BotMemStruct['powerSpawn'] &&
-            this[RESOURCE_ENERGY] >= BotMem['energy'] + BotMem['power'] * 50 &&
-            this[RESOURCE_POWER] >= BotMem['power']
+            this[RESOURCE_ENERGY] >= energy + power * 50 &&
+            this[RESOURCE_POWER] >= power
         ) {
             BotMemStruct['powerSpawn'] = true;
-            global.log(`[自动PowerSpawn] ${this.name}资源高于阈值, 已开启PowerSpawn`);
+            log('AutoPower', `${this.name}资源高于阈值, 已开启PowerSpawn`);
         }
     }
 }
