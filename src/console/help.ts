3f07013ad5163d3300e3461aeedd0bccb998d5b3
KurohaneKaoruko <kurohanekaoruko@163.com>
2026-01-17 20:12:11 +0800

    chore: 更新版本号至1.9.2

--- src/console/help.ts
+++ src/console/help.ts
@@ -1,4 +1,4 @@
-const VERSION = '1.9.1';
+const VERSION = '1.9.2';
 
 const head = `<span style="color: #D0CAE0;"><b>
 ——————————————————————————— 迷迭香 𝕽𝖔𝖘𝖒𝖆𝖗𝖎𝖓 ${VERSION} ———————————————————————————
